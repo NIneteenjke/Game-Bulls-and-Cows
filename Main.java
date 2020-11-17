6365bae3a16b388a1edbfc5c1763e7b2519e9160
NIneteenjke <73357738+NIneteenjke@users.noreply.github.com>
2020-11-17 20:41:08 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,55 +9,63 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int bull = 0;
         int cows = 0;
 
-        System.out.println("Сколько чисел вы хотите загадать?");
-        Scanner sum = new Scanner(System.in);
-        int number = sum.nextInt();
-        int[] num = new int[number];
+        System.out.println("Поробуйте угадать двузначное число");
+//        Scanner sum = new Scanner(System.in);
+//        int number = sum.nextInt();
+
+        int[] num = new int[2];
 
 
         for (int i = 0; i < num.length; i++) {
             num[i] = (int) (Math.random() * 9);
-            System.out.println(num[i] + "  ");
+            System.out.print(num[i] + "  ");
         }
-
         int[] all = new int[2];
-        boolean tryToWin = true;
 
+       boolean tryToWin = true;
         while (tryToWin) {
-            //boolean found = false;
+
             for (int i = 0; i < num.length; i++) {
                 System.out.println("Введите ваше число");
                 Scanner numbers = new Scanner(System.in);
                 int inputPlayer = numbers.nextInt();
-                int[] numbersPlayer = new int[inputPlayer];
-                if (inputPlayer == num[i]) {
-                    System.out.println("Вы отгадали число");
-                    //found = true;
+                Scanner numbersSecond = new Scanner(System.in);
+                int inputPlayer2 = numbers.nextInt();
+                int[][] numbersPlayer = new int[inputPlayer][inputPlayer2];
+                if (inputPlayer == num[0]) {
+                    System.out.println(inputPlayer + " Вы отгадали первое число ");
                     bull++;
-                    tryToWin = false;
-                    break;
+                } else if (inputPlayer == num[1]) {
+                    System.out.println(inputPlayer + " Вы отгадали второе число число");
+                    cows++;
+                }
+                if (inputPlayer2 == num[0]) {
+                    System.out.println(inputPlayer2 + " Вы отгадали первое число");
+                    cows ++;
+                } else if (inputPlayer2 == num[1]) {
+                    System.out.println(inputPlayer2 + " Вы отгадали второе число число");
+                    bull++;
                 } else {
-                    System.out.println("К сожалению это не то число, попробуйте еще раз!");
+                    System.out.println("К сожалению это не те числа, попробуйте еще раз!");
                     i++;
                 }
-
-
+                if (bull == 2) {
+                    System.out.println("Вы отгадали все числа");
+                    tryToWin = false;
+                    break;
+                }
             }
-
         }
-
         all[0] = cows;
         all[1] = bull;
-
-        System.out.println(all[0] + "Cows ");
-        System.out.println(all[1] + "Bulls ");
+        System.out.println("bull " + all[1]);
     }
 }
+
 
 
 
---
+++ TestGame.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestGame {
+    @Test
+    public void testingMas() {
+        int[] num = new int[2];
+        if (num == null) {
+            System.out.println("wrong");
+        }
+    }
+@Test
+    public void testingCondition(){
+    Main main=new Main();
+    boolean tryToWin = true;
+    Assert.assertTrue( true);
+    }
+
+}

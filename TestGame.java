c8e5448a9f51704d1ad576965088b3fa204d7f0f
NIneteenjke <73357738+NIneteenjke@users.noreply.github.com>
2020-11-17 20:49:06 +0600

    Add files via upload

--- TestGame.java
+++ TestGame.java
@@ -9,11 +9,23 @@ public class TestGame {
             System.out.println("wrong");
         }
     }
-@Test
-    public void testingCondition(){
-    Main main=new Main();
-    boolean tryToWin = true;
-    Assert.assertTrue( true);
+
+    @Test
+    public void testingCondition() {
+        Main main = new Main();
+        boolean tryToWin = true;
+        Assert.assertTrue(true);
     }
 
+    @Test
+    public void testingArrayResult() {
+        int bull=0;
+        int cows=0;
+        int[] all = new int[2];
+        all[0] = bull;
+        all[1] = cows;
+        if(all[0]==2){
+            System.out.println("ok");
+        }
+    }
 }
